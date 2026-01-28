68f382732ae1861be4cf3a9e8169e76abbf8be67
Navadeesh Mallayya Gangadhari <navdish15@gmail.com>
2026-01-29 00:58:27 +0530

    fix API_BASE error in UserFiles

--- frontend/src/pages/users/UserFiles.js
+++ frontend/src/pages/users/UserFiles.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
 import { useNavigate } from "react-router-dom";
-const API_BASE = process.env.REACT_APP_API_URL;
+
 
 const UserFiles = () => {
   const [files, setFiles] = useState([]);
